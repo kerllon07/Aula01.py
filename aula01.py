6bf51a8883420488bafcf3bffee5e8f506dafb9a
kerllon07 <129960911+kerllon07@users.noreply.github.com>
2024-03-27 10:53:20 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,15 @@
+import os
+
+#print("Olá playboyzinho")
+
+print("O 3ds é a turma numero 1 do Sagrada")
+
+turma="2ds"
+pos=2
+
+print("o" , turma , "é a turma numero ", pos,  "do sagrada ")
+
+print(type (turma) )
+print(type (pos) )
+
+os.system("cls")
